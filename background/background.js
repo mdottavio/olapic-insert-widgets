fe7d3edc4666733f69cfd048f8d652b3b8459dc4
Joel <joelalovera@gmail.com>
2014-05-24 00:24:31 -0400

    add back

---
+++ background/background.js
@@ -0,0 +1,30 @@
+Olapic = {
+    init: function() {
+
+    },
+    set: function(url, data) {
+        localStorage[url] = data;
+    },
+    get: function(url) {
+        return localStorage[url];
+    },
+    listener: function() {
+        chrome.extension.onRequest.addListener(
+            function(request, sender, sendResponse)
+            {   
+                if(request.olapic.op  === 2) {
+                    switch (request.olapic.data.action)
+                    {
+                        case "get":
+                            Olapic.get(request.olapic.data.href);
+                        break;
+                        case "set":
+                            Olapic.set(request.olapic.data.info);
+                        break;
+                    }
+                }   
+            }
+        );
+    }
+
+}
