ecfd04f7abe58381297562f606ccfbca3f0c3706
dottavio <dottavio@gmail.com>
2014-05-23 23:13:37 -0400

    Cliente, primer intento

--- client/front-client.js
+++ client/front-client.js
@@ -1,21 +1,49 @@
-//start.js
 
-var olapicExtensionClient = function(useSandbox){
-    init = function(selector){
-        // jQuery(selector).y
+(function(window, document){
+
+    var oLoader = function(){},
+        olapicBuild = '//photorankstatics-a.akamaihd.net/81b03e40475846d5883661ff57b34ece/static/frontend/latest/build.min.js';
+        instanceData = {};
+        // olapicBuild = '//local.photorank.me/sdkjs/dist/build.js';
+    
+    msgResponds = function(){
+        var _self = this;
+        try {
+            if ( e === 'undefined' || e === null || e.olapic.method === 'undefined' ) { return null; }
+            else{
+                if((e.data.search(/olapic\.magic/) > -1) ){
+                    /// let see what you need
+                    instanceData = JSON.parse(e.data);
+                    jQuery('body').eltree({
+                        click: function(wrapperData){
+                            _self.loadScripts(wrapperData);
+                        }
+                    })
+                    this.loadScripts(function(){
+
+                    });
+                }
+            }
+        } catch (er) { }
     };
 
-    if (window.addEventListener){
-        addEventListener("message", listener, false)
-    } else {
-        attachEvent("onmessage", listener)
+    loadScripts = function(wrapperData){
+        var olapicwidget = document.createElement("script");
+        olapicwidget.type = "text/javascript";
+        olapicwidget.src = olapicBuild;
+        olapicwidget.charset = "UTF-8";
+        olapicwidget.setAttribute('data-olapic', wrapperData.js);
+        olapicwidget.setAttribute('data-instance', instanceData.instance_hash);
+        olapicwidget.setAttribute('data-apikey', instanceData.apikey);
+        olapicwidget.async = true;
+        document.getElementsByTagName("head")[0].appendChild(olapicwidget);
+    };
+
+    if(typeof window.addEventListener !== 'undefined') {
+        window.addEventListener('message', this.msgResponds, false);
+    } else if(typeof window.attachEvent !== 'undefined') {
+        window.attachEvent('onmessage', this.msgResponds);
     }
 
 
-    return {
-        init : init,
-        insertWidget : insertWidget
-    }
-};
-
-oeClient = olapicExtensionClient();
+})(window, document)
